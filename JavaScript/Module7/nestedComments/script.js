76376d3653d2cb79a5e8e1504de29333a3516355
pavankumar <pavankumarkosaraju@gmail.com>
2024-09-29 19:54:37 +0530

    updated the javaScript

---
+++ JavaScript/Module7/nestedComments/script.js
@@ -0,0 +1,48 @@
+const submitBtn = document.querySelector("#submitComment");
+const commentInput = document.querySelector("#commentInput");
+const commentsContainer = document.querySelector(".commentsContainer");
+
+submitBtn.addEventListener("click", (e) => {
+    // Get the value from the input box
+    const commentText = commentInput.value.trim();
+
+    // Add a comment with that value
+    addComment(commentText);
+
+    // Clear the text in the input box
+    commentInput.value = '';
+});
+
+function addComment(text) {
+    console.log("adding a new comment with text", text);
+
+    const commentDiv = document.createElement("div");
+    commentDiv.setAttribute("class", "comment");
+
+    commentDiv.innerHTML = `
+        <p>${text}</p>
+        <button class="replyButton">Reply</button>
+        <div class="repliesContainer"></div>
+        <textarea class="replyInput" placeholder="Write a reply.."></textarea>
+    `;
+
+    commentsContainer.appendChild(commentDiv);
+}
+
+commentsContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("replyButton")) {
+        // Get the reply input value for this comment
+        const commentParent = e.target.parentElement;
+        const replyInput = commentParent.querySelector(".replyInput"); 
+        const repliesContainer = commentParent.querySelector(".repliesContainer");
+        const replyValue = replyInput.value.trim();
+
+        if (replyValue) { 
+            const reply = document.createElement('p');
+            reply.innerText = replyValue;
+            repliesContainer.appendChild(reply);
+
+            replyInput.value = "";
+        }
+    }
+});
